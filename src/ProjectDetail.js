6b05ace4a0050bb5ecb3f841a697b5f8803b0000
antoniodenuevo <131149348+antoniodenuevo@users.noreply.github.com>
2024-07-10 15:42:45 +0100

    Add external link button to Project 4

--- src/ProjectDetail.js
+++ src/ProjectDetail.js
@@ -159,7 +159,7 @@ Videos were sourced from personal archives and Spanish TV footage from the 1990s
               rel="noopener noreferrer"
               className="external-link-button"
             >
-              <p>Watch Echoes Of Sadness</p>
+              <p>watch echoes of sadness</p>
             </a>
           )}
         </div>
